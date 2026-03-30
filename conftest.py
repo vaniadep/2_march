981a7e8b85c87417ada00d69d9c6bcfb1cb8c194
vaniadep <149788717+vaniadep@users.noreply.github.com>
2026-03-30 22:49:41 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -11,7 +11,6 @@ def base_url():
 
 @pytest.fixture(scope="session")
 def api_session(base_url):
-    """Сессия requests для эффективного выполнения запросов."""
     session = requests.Session()
     session.base_url = base_url
     yield session
@@ -20,7 +19,6 @@ def api_session(base_url):
 
 @pytest.fixture
 def get_user(api_session):
-    """Фабрика-фикстура для получения пользователя по ID."""
     def _get_user(user_id: int) -> UserSchema:
         response = api_session.get(f"{api_session.base_url}/users/{user_id}")
         response.raise_for_status()
